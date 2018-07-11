4970237264c99c9c55c1a3887624720400d3bef8
Tristan Macell <tristanmacelli@gmail.com>
2018-07-11 09:44:19 -0700

    add react-intro changes

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,1 +1,58 @@
 import React, { Component } from 'react';
+
+class HelloMessage extends Component {
+    render() {
+        let message = "Hooray!";
+        return <h1>{message}</h1>;
+    }
+}
+
+class ButtonList extends Component {
+    render() {
+
+        let buttonArray = this.props.labels.map((label) => {
+            return <Button key={label} text={label} size="lg" />;
+        });
+
+        return (
+            <div>
+                {buttonArray}
+            </div>
+        );
+    }
+}
+
+class Button extends Component {
+    render() {
+        // console.log(this.props);
+
+        let className = "btn btn-primary rounded-circle";
+        if (this.props.size === 'lg') {
+            className += ' btn-lg';
+        }
+
+        return (
+            <button className={className}>
+                {this.props.text}
+            </button>
+        );
+    }
+}
+
+export default class App extends Component {
+    render() {
+
+        let buttonLabels = [1, 2, 3];
+
+        return (
+            <div>
+                <header className="jumbotron">
+                    <HelloMessage content={<h1>hello</h1>} />
+                </header>
+                <main>
+                    <ButtonList labels={buttonLabels} />
+                </main>
+            </div>
+        );
+    }
+}
--- react-intro/src/index.js
+++ react-intro/src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
+// import './style.css'; lets you make your own css & you do not need to include the stylesheet
+// in your html file!
+
+ReactDOM.render(<App />, document.getElementById('root'));
